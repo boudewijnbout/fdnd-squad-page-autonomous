7b3a4205318c9764f0b9699a994762d8aa1b5d0e
DphnZwp <zwuupdaphne@gmail.com>
2021-09-19 23:00:54 +0200

    Popup animatie
    Animatie van de popup gemaakt/getest en Nandita's quote toegevoegd.

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function openBox() {
+    document.getElementById("myPopup").style.display = "block";
+  }
+  
+  function closeBox() {
+    document.getElementById("myPopup").style.display = "none";
+  }
