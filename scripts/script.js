c6227c4a7e6d1ab77325e3e986d60f7b483b6e1e
M4TThys123 <70703948+M4TThys123@users.noreply.github.com>
2021-09-21 13:29:14 +0200

    klaar voor de merge

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,15 @@
-// JavaScript Document
-console.log("howdy");
-
-// Filter Systeem
-// Bron https://codepen.io/shooft/pen/VwpvVYy
 var optieAll = document.querySelector("#filter-all");
 var optieTeam1 = document.querySelector("#filter-team1");
 var optieTeam2 = document.querySelector("#filter-team2");
 var optieTeam3 = document.querySelector("#filter-team3");
 
 function filterLijst(event){
-  let deLijst = document.querySelector("main > ul");
+  let deLijst = document.querySelector("#list");
   let nieuweFilter = event.target.value;
   deLijst.className = "";
   deLijst.classList.add(nieuweFilter);
 }
 
-/* de radio buttons laten luisteren naar wijzigingen */
-/* dan de functie filterList uitvoeren */
 optieAll.addEventListener("change", filterLijst);
 optieTeam1.addEventListener("change", filterLijst);
 optieTeam2.addEventListener("change", filterLijst);
